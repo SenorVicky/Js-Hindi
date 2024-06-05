debf251f68c0ac17440fed8a2ecb3a0618321d82
SenorVicky <123752276+SenorVicky@users.noreply.github.com>
2024-06-05 11:04:07 +0000

    First Topic on Variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,24 @@
+const accountId=144663
+let accountEmain="vck@gmail.com"
+var accountPassword="12456"
+accountCity="Aligarh"
+
+let accountState;
+
+
+// accountId=2  // Not Allowed
+
+accountEmain="aks@gmail.com"
+
+accountPassword="6342"
+
+accountCity="Bihar"
+
+console.log(accountId)
+
+/*
+Prefer Not to Use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmain,accountPassword,accountCity,accountState])
