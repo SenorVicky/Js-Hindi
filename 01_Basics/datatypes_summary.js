8a6556bfe83d4944da0520c33850efd37f1e8ec8
SenorVicky <123752276+SenorVicky@users.noreply.github.com>
2024-06-06 07:21:41 +0000

    DataTypes Summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,37 @@
+// Primitive    
+        
+    //7 Types :String, Number, Boolean, null, undefined, Symbol, BigInt
+
+    const score=100
+    const scoreValue=100.3
+    const isLoggedIn=true
+    const outsideTemp=null
+    let userEmail;
+
+
+    const id=Symbol('123')
+    // const anotherId=Symbol('123')
+
+    // console.log(id===anotherId)
+
+
+    const bigNumber=3545234534523452346454354n // BigInt
+
+
+// Reference Type(Non - Primitive)
+
+    // Array, Objects, Functions
+
+    const heroes=["IronMan", "SuperMan", "Thor","Vikash"];
+    let myObj={
+        name:"Vikash Rajput",
+        age: 23
+    }
+
+    const myFun=function() {
+        console.log("Hello World")
+    }
+
+    console.log(typeof id)
+
+ 
