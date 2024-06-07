2a7f8aaa32961d1698f43a9bca8727f80f9e7c75
SenorVicky <123752276+SenorVicky@users.noreply.github.com>
2024-06-07 07:52:09 +0000

    Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,26 @@
+const name="vikash"
+const repoCount=50
+
+// console.log(name+repoCount+" Value") // Outdated syntex
+
+// console.log(`Hello My Name is ${ name} and My Repo Count is ${repoCount}`)
+
+// console.log("hello")
+
+// const game=new String("Vikash-hp")
+
+// console.log(game.toUpperCase())
+
+// const newStr =game.substring(6,9)
+
+// console.log(newStr)
+
+// let url="https://hacker.com/hack%20vikash"
+
+// console.log(url.replace('%20','-')) 
+
+// console.log(url.includes("vikash"))
+
+// const st="Hacker-Nothing-Hello"
+
+// console.log(st.split("-"))
