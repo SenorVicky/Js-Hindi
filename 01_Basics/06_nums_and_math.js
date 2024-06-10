de8473afe68ae7eb89be6fab8d32970258a4d129
SenorVicky <123752276+SenorVicky@users.noreply.github.com>
2024-06-10 15:59:26 +0000

    Date And Time

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,33 @@
+// const score=400
+
+// const balance=new Number(100)
+
+// // console.log(`${score} And ${balance}`) 
+
+// console.log(balance.toString().length) 
+// console.log(balance.toFixed(1)) 
+
+// const otherNum=12334.8979
+
+// console.log(otherNum.toPrecision(5))
+
+console.log(Math)
+
+const n=4
+
+// console.log(Math.floor(4.6))
+
+// console.log(Math.sqrt(n))
+
+
+// console.log(Math.random()*10+1)
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)))
+
+console.log(Math.floor((Math.random()*(max-min+1))+min))
+
+
+
