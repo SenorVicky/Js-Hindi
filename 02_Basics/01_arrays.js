f9abb5caadaa07d7e9fb35b97d2536ec0b58b468
SenorVicky <123752276+SenorVicky@users.noreply.github.com>
2024-06-11 06:49:45 +0000

    arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,44 @@
+// array
+
+const myArr=[0,1,2,3,4,5]
+
+// const myHeroes=["IronMan", "Thor","SpiderMan", "Captain America"]
+
+// const myArr2=new Array(1,2,3,5)
+
+// console.log(myArr)
+
+//Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9) // Add 9 at start of the array
+// myArr.shift()  // Remove first element
+
+
+// console.log(myArr.includes(5))
+// console.log(myArr.indexOf(4))
+
+// const newArr= myArr.join()
+
+// console.log(myArr)
+
+// console.log(typeof(newArr))
+
+// slice , splice
+
+console.log("A ", myArr);
+
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+
+console.log("B ",myArr);
+
+console.log("*************************************");
+
+const myn2=myArr.splice(1,3)
+console.log("C ",myn2);
+
+console.log(myArr); 
